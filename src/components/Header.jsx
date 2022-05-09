1f8fe5b0a4cd7acb7b85a03bf4a29dcf9be58134
PauloMisaki <ph.sorata@gmail.com>
2022-05-09 18:09:50 -0300

    Requisito 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Sistema Solar</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
